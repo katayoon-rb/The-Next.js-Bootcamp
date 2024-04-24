bd092603933ff20e9189032e87e91da49a40c143
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-04-24 18:45:30 +0330

    restaurant layout

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,17 +1,14 @@
-import Header from "../components/Header";
 import RestaurantNavbar from "../components/RestaurantNavbar";
 import Menu from "../components/Menu";
+import RestaurantLayout from "../RestaurantLayout";
 
 export default function RestaurantMenuPage() {
   return (
-    <>
-      <Header />
-      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-        <div className='bg-white  />w-[100%] rounded p-3 shadow'>
-          <RestaurantNavbar />
-          <Menu />
-        </div>
+    <RestaurantLayout>
+      <div className='bg-white w-[100%] rounded p-3 shadow'>
+        <RestaurantNavbar />
+        <Menu />
       </div>
-    </>
+    </RestaurantLayout>
   );
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,15 +1,12 @@
-import Header from "./components/Header";
 import Description from "./components/Description";
 import ReservationCard from "./components/ReservationCard";
+import RestaurantLayout from "./RestaurantLayout";
 
 export default function RestaurantDetails() {
   return (
-    <>
-      <Header />
-      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-        <Description />
-        <ReservationCard />
-      </div>
-    </>
+    <RestaurantLayout>
+      <Description />
+      <ReservationCard />
+    </RestaurantLayout>
   );
 }
