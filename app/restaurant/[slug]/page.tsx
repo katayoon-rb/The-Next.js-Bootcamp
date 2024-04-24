2f24c0ecd967ecb51540f971923fc708c3d4662f
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-04-24 18:33:19 +0330

    moving universally used components to the root layout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,19 +1,15 @@
-import Navbar from "@/components/Navbar";
 import Header from "./components/Header";
 import Description from "./components/Description";
 import ReservationCard from "./components/ReservationCard";
 
 export default function RestaurantDetails() {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <Navbar />
-        <Header />
-        <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-          <Description />
-          <ReservationCard />
-        </div>
-      </main>
-    </main>
+    <>
+      <Header />
+      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
+        <Description />
+        <ReservationCard />
+      </div>
+    </>
   );
 }
