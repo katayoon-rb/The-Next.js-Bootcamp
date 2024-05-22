efa7926a672e1c85ebbc7b0004b9860d9655edf6
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-05-22 15:47:24 +0330

    debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "react-datepicker/dist/react-datepicker.css";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import AuthContext from "@/context/AuthContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,12 +20,10 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <main className='bg-gray-100 min-h-screen w-screen'>
-          <AuthContext>
-            <main className='max-w-screen-2xl m-auto bg-white'>
-              <Navbar />
-              {children}
-            </main>
-          </AuthContext>
+          <main className='max-w-screen-2xl m-auto bg-white'>
+            <Navbar />
+            {children}
+          </main>
         </main>
       </body>
     </html>
